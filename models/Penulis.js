8fb12aa84728f80e6fd489b79860c849b0454596
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-08-05 14:29:47 +0700

    menambahkan Field Email dan Password

--- models/Penulis.js
+++ models/Penulis.js
@@ -8,6 +8,15 @@ module.exports = (sequelize, DataTypes) => {
         nama: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
         }
     }, {
         tableName: 'penulis',
