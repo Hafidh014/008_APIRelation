20b73366b5cfc1e7902c26ce10750fbabbe7a63e
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-08-05 15:05:06 +0700

    Implementasi Fungsi getAll dengan Eager Loading

--- controller/komikController.js
+++ controller/komikController.js
@@ -4,3 +4,31 @@ const Komik = db.Komik;
 const Penulis = db.Penulis;
 const Genre = db.Genre;
 
+async function getAll(req, res) {
+    try {
+        const komik = await Komik.findAll({
+            include: [
+                {
+                    model: Penulis,
+                    as: "penulis",
+                    attributes: ["id", "nama", "email"]
+                },
+                {
+                    model: Genre,
+                    as: "genres",
+                    attributes: ["id", "nama"],
+                    through: {
+                        attributes: []
+                    }
+                }
+            ]
+        });
+
+        return res.status(200).json(komik);
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
